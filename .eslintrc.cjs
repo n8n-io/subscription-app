16dee253b826b9f5be9c13050c1631376dd1e456
Mutasem <mutdmour@gmail.com>
2022-12-01 17:27:38 +0100

    chore: lint fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,4 +18,5 @@ module.exports = {
 	parserOptions: {
 		ecmaVersion: 'latest',
 	},
+	ignorePatterns: ['commitlint.config.js'],
 };
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-
 </script>
 
 <template>
-  <el-config-provider size="small" :z-index="3000">
+	<el-config-provider size="small" :z-index="3000">
 		<header>
-			<img alt="n8n logo" class="logo" src="@/assets/logo.png" height="25" />
+			<img
+				alt="n8n logo"
+				class="logo"
+				src="@/assets/logo.png"
+				height="25"
+			/>
 		</header>
 
 		<RouterView />
-  </el-config-provider>
+	</el-config-provider>
 </template>
 
 <style module></style>
