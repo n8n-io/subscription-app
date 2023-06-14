56867645ac2c2e13df4283ca0f0db2fa11ba2b1d
Cornelius Suermann <cornelius.suermann@gmail.com>
2023-06-14 14:28:44 +0200

    adjust startup plan description

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -18,7 +18,7 @@ export default {
 		'For larger companies with compliance requirements',
 	'plan.startup': 'Startup',
 	'plan.startup.description':
-		'For startups with up to 50 employees that raised up to $5M',
+		'For startups with up to 20 employees that raised up to $5M',
 	'feature.ldap': 'LDAP',
 	'feature.saml': 'SSO SAML and LDAP',
 	'feature.variables': 'Global variables',
